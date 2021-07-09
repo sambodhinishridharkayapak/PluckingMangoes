0e1a3a9ba9c92bd9c1242b9f2324bf2c58429857
sambodhinishridharkayapak <78585690+sambodhinishridharkayapak@users.noreply.github.com>
2021-07-09 16:26:15 +0530

    Add files via upload

---
+++ launcher.js
@@ -0,0 +1,37 @@
+class launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 100
+        }
+       // this.sling1 = loadImage('sprites/sling1.png');
+       // this.sling2 = loadImage('sprites/sling2.png');
+        //this.sling3 = loadImage('sprites/sling3.png');
+        this.pointB = pointB;
+        this.bodyA = bodyA;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    attach(body){
+    this.sling.bodyA = body;
+    }
+    
+    fly(){
+     this.sling.bodyA = null;
+    }
+
+    display(){
+        //image(this.sling1,200,20);
+        //image(this.sling2,170,20);
+        if(this.sling.bodyA){
+
+          var pointA = this.bodyA.position;
+          var pointB = this.pointB;
+
+          line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
